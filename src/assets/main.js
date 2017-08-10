8b1dacd6fcd3526ebbe73676901be6456a32d3e6
collin-patterson <30846504+collin-patterson@users.noreply.github.com>
2017-08-10 11:14:40 -0500

    update

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,20 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+
+    if(answer.value == "" || attempt.value == "") {
+      setHiddenFields();
+    }
 }
 
-//implement new functions here
+function setHiddenFields() {
+  answer.value = Math.floor(Math.random() * 10000).toString();
+  while(answer.value.length < 4) {
+    answer.value = "0" + answer.value;
+  }
+  attempt.value = "0";
+}
+
+function setMessage(message) {
+    document.getElementById('message').innerHTML = message;
+}
